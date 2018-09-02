74b13f394449d08881e251ccdc897cfea043c45c
enoch <qyqdream@gmail.com>
2018-09-03 00:55:05 +0800

    upload: 在项目中使用 vuex & auth 模块

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
-  <header :class="[isLogin?'common':'sticky']">
+  <header :class="[isLogin?'sticky':'common']">
     <template v-if=!isLogin>
       <h1>博客小站</h1>
       <p>Let's Share !</p>
       <div class="btns">
-        <el-button round>立即登录</el-button>
-        <el-button round>注册账号</el-button>
+        <router-link to="/login">
+          <el-button round>立即登录</el-button>
+        </router-link>
+        <router-link to="/register">
+          <el-button round>注册账号</el-button>
+        </router-link>
       </div>
     </template>
     <template v-if=isLogin>
@@ -13,18 +17,42 @@
       <p>Let's Share !</p>
       <div class="btns">
         <i class="edit el-icon-edit"></i>
-        <el-button round>退出登录</el-button>
+        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username"> 
+        <el-button round @click="onLogout">退出登录</el-button>
       </div>
     </template>
   </header>
 </template>
 
 <script>
+  
+  import { mapGetters, mapActions } from 'vuex'
+
+  import auth from '@/api/auth'
+  window.auth = auth
+
   export default {
     data(){
-      return {
-        isLogin: true
-      }
+      return {}
+    },
+    computed: {
+      ...mapGetters([
+        'isLogin',
+        'user'
+      ])
+    },
+    created() {
+      this.checkLogin() 
+    },
+    methods: {
+      ...mapActions([
+        'checkLogin',
+        'logout'
+      ]),
+
+      onLogout() {
+        this.logout()
+      },
     }
   }
 </script>
@@ -68,6 +96,14 @@ header.sticky{
   }
   .btns{
     margin-right:20px;
+    .avatar{
+      width:40px;
+      vertical-align:top;
+      margin:0 10px;
+    }
+    button{
+      margin:0 10px;
+    }
   }
 }
 </style>
