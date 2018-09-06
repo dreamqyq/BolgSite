110064731192d16497fed0fac8523d2a994a2d79
enoch <qyqdream@gmail.com>
2018-09-06 14:07:48 +0800

    update: Creat/Edit input length limit function

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -7,9 +7,27 @@ export default {
       description: '',
       content: '',
       atIndex: false,
-      limit: 0
+      titleLimit: {
+        limit: 30,
+        isLimit: false
+      },
+      descriptionLimit: {
+        limit: 100,
+        isLimit: false
+      },
+      contentLimit: {
+        limit: 10000,
+        isLimit: false
+      }
     }
   },
+
+  created () {
+  },
+
+  computed: {
+  },
+
   methods: {
     onCreate () {
       blog.createBlog({ title: this.title, description: this.description, content: this.content, atIndex: this.atIndex })
@@ -19,7 +37,13 @@ export default {
         })
     },
     limitNumber (num, selector) {
-      this.limit = num - this[selector].length
+      let limitSelector = selector + 'Limit'
+      this[limitSelector].limit = num - this[selector].length
+      if (this[limitSelector].limit < 0) {
+        this[limitSelector].isLimit = true
+      } else {
+        this[limitSelector].isLimit = false
+      }
     }
   }
 }
--- src/pages/Create/template.vue
+++ src/pages/Create/template.vue
@@ -2,14 +2,16 @@
   <div id="create">
     <h1>创建文章</h1>
     <h3>文章标题</h3>
-    <el-input v-model="title" @input="limitNumber(30,'title')"></el-input>
-    <p class="msg">字数限制在30字内，您还可以输入 {{ limit }} 字</p>
+    <el-input v-model="title" @input="limitNumber(30, 'title')" :class="{active: titleLimit.isLimit}" clearable></el-input>
+    <p :class="[{ active: titleLimit.isLimit }, 'msg']">字数限制在 30 字内，您还可以输入 {{ titleLimit.limit }} 字</p>
     <h3>内容简介</h3>
-    <el-input type="textarea" :autosize="{minRows: 2,maxRows: 6}" v-model="description"></el-input>
-    <p class="msg">限100个字</p>
+    <el-input type="textarea" :autosize="{minRows: 2,maxRows: 6}" v-model="description" 
+      @input="limitNumber(100, 'description')" :class="{active: descriptionLimit.isLimit}"></el-input>
+    <p :class="[{ active: descriptionLimit.isLimit }, 'msg']">字数限制在 100 字内，您还可以输入 {{ descriptionLimit.limit }} 字</p>
     <h3>文章内容</h3>
-    <el-input type="textarea" :autosize="{minRows: 4,maxRows: 30}" v-model="content"></el-input>
-    <p class="msg">限10000个字</p>
+    <el-input type="textarea" :autosize="{minRows: 4,maxRows: 30}" v-model="content"
+      @input="limitNumber(10000, 'content')" :class="{active: contentLimit.isLimit}"></el-input>
+    <p :class="[{ active: contentLimit.isLimit }, 'msg']">字数限制在 10000 字内，您还可以输入 {{ contentLimit.limit }} 字</p>
     <p class="btn">
       <label>是否展示到首页</label>
       <el-switch
